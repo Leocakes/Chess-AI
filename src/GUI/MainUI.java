d9580b66d50e2c6c8abe72f1e69ac4d1a7a07e6f
leocakes <leocakes@DESKTOP-K7G2LF2>
2018-01-10 21:17:23 -0500

    fixed stuff

--- src/GUI/MainUI.java
+++ src/GUI/MainUI.java
@@ -19,9 +19,10 @@ public class MainUI {
         System.out.println("Play aganist an ai. GLHF :)");
         helpUI();
         Game();
-    } //blank
+    } //starts the user interface
     
      public void Game(){
+        int depth = 4;
         Boolean running = true;
         Scanner scan = new Scanner(System.in);
         Board board = null;
@@ -33,54 +34,67 @@ public class MainUI {
                     helpUI();
                     break;
                 case "difficulty": //changes difficulty
-                    int difficulty = Integer.parseInt(args[1]);
-                    // add call to change difficulty
+                    try{
+                        int difficulty = Integer.parseInt(args[1]);
+                        if (difficulty < 6 && difficulty > 2){
+                            depth = difficulty;
+                        } else {
+                            System.out.println("Please provide a number from 3-5.");
+                        }
+                    } catch (Exception e){
+                        System.out.println("Please provide an integer for the difficulty.");
+                    }
                     break;
                 case "new": //starts a new game
                     board = new Board("data/board.chs");
                     board.Print();
                     break;
-                case "load":
+                case "load": //loads a saved game
                     board = new Board("data/" + args[1]); // loads 
+                    System.out.println();
                     board.Print();
                     break;
                 case "save":
                     if (board != null){
-                        board.saveGame(args[1] + ".chs");
+                        board.saveGame(args[1]);
                     } else {
                         System.out.println("Cannot save a non-existing game.");
                     }
                     break;
                 case "move":
                     if (board != null){
-                        String []oldpos = args[1].split("");
-                        int y = Integer.parseInt(oldpos[1]) - 1;
-                        int x = getPosition(oldpos[0]);
-                        Piece currentPiece = board.boardArray[x][y];
-                        if (currentPiece!=null) {
-                            String []newpos = args[2].split("");
-                            Move m = currentPiece.getMove(new Point(getPosition(newpos[0]), Integer.parseInt(newpos[1]) - 1));
-                            if (m != null){
-                                board.doMove(m);
-                                System.out.println();
-                                System.out.println();
-                                board.Print();
-                                Tree tree = new Tree(board,false);
-                                board.doMove(tree.getNext());
-                                board.Print();
-                                System.out.println();
+                        try{
+                            String []oldpos = args[1].split("");
+                            int y = Integer.parseInt(oldpos[1]) - 1;
+                            int x = getPosition(oldpos[0]);
+                            Piece currentPiece = board.boardArray[x][y];
+                            if (currentPiece!=null) {
+                                String []newpos = args[2].split("");
+                                Move m = currentPiece.getMove(new Point(getPosition(newpos[0]), Integer.parseInt(newpos[1]) - 1));
+                                if (m != null){
+                                    board.doMove(m);
+                                    System.out.println();
+                                    board.Print();
+                                    System.out.println();
+                                    Tree tree = new Tree(board,false,depth);
+                                    board.doMove(tree.getNext());
+                                    board.Print();
+                                    System.out.println();
+                                } else {
+                                    System.out.println(args[2] + " is not a valid move. Please try again.");
+                                }
                             } else {
-                                System.out.println(args[2] + " is not a valid move. Please try again.");
+                                System.out.println("Not a piece");
                             }
-                        } else {
-                            System.out.println("Not a piece");
-                        }
-                        //Check for Checkmate
-                        List<Move> moves = board.fetchMoves(Side.White);
-                        for(Move m : moves) {
-                            if (m.piece.getPiece(m.move) instanceof Main.Pieces.King) {
-                                board.doMove(m);
+                            //Check for Checkmate
+                            List<Move> moves = board.fetchMoves(Side.White);
+                            for(Move m : moves) {
+                                if (m.piece.getPiece(m.move) instanceof Main.Pieces.King) {
+                                    board.doMove(m);
+                                }
                             }
+                        } catch (Exception e){
+                            System.out.println("Please provide proper parameters. \nExample, move a2 a3");
                         }
                     } else {
                         System.out.println("Please start a game.");
@@ -103,9 +117,7 @@ public class MainUI {
             System.out.print("Mem used:");
             System.out.println((run.totalMemory() - run.freeMemory())/1000000);
         }
-        
-    
-    }
+    } // play of the game
      
     public int getPosition(String letter){
         switch(letter.toLowerCase()){
@@ -125,20 +137,19 @@ public class MainUI {
                 return 6;
             case "h":
                 return 7;
-        }
+        } // converts letter to number for the array
         System.out.println("Please provide a proper position.");
         return 10;
-    }
+    } // converter
     
     public void helpUI(){
         System.out.println("help                         shows the help page");
-        System.out.println("difficulty x                 set the difficulty, where x is the difficulty level (3-7), default 5");
+        System.out.println("difficulty x                 set the difficulty, where x is the difficulty level (3-5), default 4");
         System.out.println("new game                     starts a new game");
         System.out.println("load filename                load a game file");
         System.out.println("save filename                save the current game to a file");
         System.out.println("move oldposition newpostion  where oldposition is the position of the current piece you want to move and newposition is where the piece will be moved to. Example, move a2 a3");
         System.out.println("exit                         exits the game");
-    }
+    } // help 
     
-    //dana does ascii ui shiat    
 }
